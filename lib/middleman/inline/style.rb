cf3c71d57c29c6a9c64649a2705df2e1f8124e9b
Trenton Broughton <trenton@devpie.com>
2014-09-03 16:41:12 -0400

    Initial Commit

---
+++ lib/middleman/inline/style.rb
@@ -0,0 +1,9 @@
+require "middleman/inline/style/version"
+
+module Middleman
+  module Inline
+    module Style
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/middleman/inline/style/version.rb
@@ -0,0 +1,7 @@
+module Middleman
+  module Inline
+    module Style
+      VERSION = "0.0.1"
+    end
+  end
+end
